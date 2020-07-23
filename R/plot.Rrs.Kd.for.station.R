b9d3185f4c69fd862fd7bd9dea811c3e84a7e047
raphael <raphael.mabit@gmail.com>
2020-07-23 23:35:41 +0000

    Bad Loess fit can overstretch y axis making graph useless, now take linear fit to scale y axis

--- R/plot.Rrs.Kd.for.station.R
+++ R/plot.Rrs.Kd.for.station.R
@@ -46,7 +46,7 @@ plot.Rrs.Kd.for.station <- function(path="./", depthEdZ = NA) {
   ix.waves = which(waves > 370)
   plot(waves[ix.waves], cops$Rrs.0p[ix.waves], xlab="Wavelength",
        ylab="Rrs0+",
-       ylim=c(0,max(cops$Rrs.0p.linear[ix.waves],na.rm=T)+0.001),
+       ylim=c(0,max(cops$Rrs.0p.linear[ix.waves],na.rm=T)+0.002),
        xlim=c(370, max(waves)+10),
        lwd=2, type="l", main=getwd(), sub=cops$date.mean)
   for (i in 1:nf){
@@ -63,7 +63,7 @@ plot.Rrs.Kd.for.station <- function(path="./", depthEdZ = NA) {
 
   load(paste(listfile[1], ".RData", sep=""))
   plot(waves[ix.waves], cops$Rrs.0p[ix.waves], xlab="Wavelenght", ylab="Rrs0+",
-       ylim=c(0,max(cops$Rrs.0p[ix.waves],na.rm=T)+0.001),
+       ylim=c(0,max(cops$Rrs.0p.linear[ix.waves],na.rm=T)+0.002),
        xlim=c(370, max(waves)+10),
        lwd=2, type="l", main=getwd(), sub=cops$dates[1])
   for (i in 1:nf){
